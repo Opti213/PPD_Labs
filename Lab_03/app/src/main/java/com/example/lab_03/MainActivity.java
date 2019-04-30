74bf468a28e2136dacc525d30497c24043b98ec2
opti <opti213@mail.ru>
2019-04-30 06:58:40 +0300

    lab_03 fixed db

--- Lab_03/app/src/main/java/com/example/lab_03/MainActivity.java
+++ Lab_03/app/src/main/java/com/example/lab_03/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.lab_03;
 
+import android.arch.persistence.room.Room;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.lab_03.DataBases.AppDatabase;
+import com.example.lab_03.Interfaces.StudentDao;
+import com.example.lab_03.Types.Student;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getWindow().setStatusBarColor(Color.RED);
+
+        //db connection
+        AppDatabase db =  Room.databaseBuilder(this, AppDatabase.class, "StudentsDB").allowMainThreadQueries().build();
+        StudentDao dao = db.studentDao();
+
+        addTestStudents(5, dao);
+        ArrayList<Student> arr = (ArrayList<Student>) dao.getAll();
+
+    }
+
+    void addTestStudents(int count, StudentDao dao){
+        for (int i=0; i < count;i++){
+            dao.insert(new Student(i,"testStudent", "now"));
+        }
     }
 }

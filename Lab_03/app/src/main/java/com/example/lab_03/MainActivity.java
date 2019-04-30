5b3f19e4bc77e5eeb62f911cbd909ec69440c45f
opti <opti213@mail.ru>
2019-04-30 07:24:47 +0300

    make todo for testing

--- Lab_03/app/src/main/java/com/example/lab_03/MainActivity.java
+++ Lab_03/app/src/main/java/com/example/lab_03/MainActivity.java
@@ -5,6 +5,9 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.example.lab_03.DataBases.AppDatabase;
 import com.example.lab_03.Interfaces.StudentDao;
@@ -13,7 +16,7 @@ import com.example.lab_03.Types.Student;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MyActivity";
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +29,25 @@ public class MainActivity extends AppCompatActivity {
         AppDatabase db =  Room.databaseBuilder(this, AppDatabase.class, "StudentsDB").allowMainThreadQueries().build();
         StudentDao dao = db.studentDao();
 
-        addTestStudents(5, dao);
-        ArrayList<Student> arr = (ArrayList<Student>) dao.getAll();
-
+        //test db
+        //addTestStudents(5, dao);  //todo
+        //ArrayList<Student> arr = (ArrayList<Student>) dao.getAll();
+        //ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, studentToString(arr));
+        //listView = findViewById(R.id.checkView);
+        //listView.setAdapter(itemsAdapter);
     }
 
     void addTestStudents(int count, StudentDao dao){
         for (int i=0; i < count;i++){
             dao.insert(new Student(i,"testStudent", "now"));
         }
+    }
+
+    ArrayList<String> studentToString(ArrayList<Student> students){
+        ArrayList<String> res = null;
+        for (Student student : students){
+            res.add(student.toString());
+        }
+        return res;
     }
 }
